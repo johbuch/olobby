a5672334a8d4bc1ab3666f16434c8318b29c921b
Maxime Gruchet <maxime.gruchet@gmail.com>
2020-11-30 15:50:25 +0100

    ajout du responsive du header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,13 +15,35 @@ import './header.scss';
 const Header = () => (
   <div className="header">
     <Navbar collapseOnSelect expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+
+      <div className="header__notifications">
+        <span className="header__notifications__number">2</span>
+        <MdNotifications />
+      </div>
+
+      <Navbar.Brand href="#home">O'Lobby</Navbar.Brand>
+
+      <div className="header__profil--responsive">
+        <Dropdown>
+          <Dropdown.Toggle id="dropdown-basic">
+            <Image src="" roundedCircle />
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Mon compte</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Se déconnecter</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
       <Navbar.Collapse>
+
         <Form inline>
           <FormControl type="text" placeholder="Rechercher un jeux, un profil de joueur, une plateforme" className="mr-sm-2" />
           <Button> <MdSearch /> </Button>
         </Form>
+
         <div className="header__profil">
           <div className="header__profil__notifications">
             <span className="header__profil__notifications__number">2</span>
@@ -39,10 +61,6 @@ const Header = () => (
           </Dropdown>
         </div>
 
-        <Nav defaultActiveKey="/home" className="flex-column">
-          <Nav.Link href="#!">Mon compte</Nav.Link>
-          <Nav.Link href="#!">Se déconnecter</Nav.Link>
-        </Nav>
       </Navbar.Collapse>
     </Navbar>
   </div>
