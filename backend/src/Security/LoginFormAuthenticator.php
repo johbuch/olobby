79bb8bf45d19c12d305476d435c6dae8a6b8e5ed
johbuch <johan.buchert@gmail.com>
2020-11-30 17:33:32 +0100

    test de controler

--- backend/src/Security/LoginFormAuthenticator.php
+++ backend/src/Security/LoginFormAuthenticator.php
@@ -98,7 +98,8 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        // return new RedirectResponse($this->urlGenerator->generate('main_show'));
+        
+         return new RedirectResponse($this->urlGenerator->generate('main_show'));
     }
 
     protected function getLoginUrl()
