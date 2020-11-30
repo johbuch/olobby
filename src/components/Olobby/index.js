37e1bb6642807a63b03a42bb55a5dfa16de6efe4
Maxime Gruchet <maxime.gruchet@gmail.com>
2020-11-30 13:53:03 +0100

    Mise en place du composant Olobby

---
+++ src/components/Olobby/index.js
@@ -0,0 +1,14 @@
+// == Import npm
+import React from 'react';
+
+// == Import css
+import './styles.scss';
+
+// == Composant
+const Olobby = () => (
+  <div className="app">
+  </div>
+);
+
+// == Export
+export default App;
