6b60f81e9a716a7d9cb0ae4d92b87171b9aa6116
Maxime Gruchet <maxime.gruchet@gmail.com>
2020-12-01 11:16:45 +0100

    Ajout de la liste de mes amis, mes dernier jeux et mes plateformes dans le header responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,9 +5,14 @@ import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import Dropdown from 'react-bootstrap/Dropdown';
-import Nav from 'react-bootstrap/Nav';
 
-import { MdSearch, MdNotifications } from 'react-icons/md';
+import {
+  MdSearch,
+  MdNotifications,
+  MdPerson,
+  MdMessage,
+} from 'react-icons/md';
+import { FaPlaystation } from 'react-icons/fa';
 
 // == Import scss
 import './header.scss';
@@ -26,7 +31,7 @@ const Header = () => (
       <div className="header__profil--responsive">
         <Dropdown>
           <Dropdown.Toggle id="dropdown-basic">
-            <Image src="" roundedCircle />
+            <Image src="https://androidgadget.org/wp-content/uploads/2020/03/Regardez-la-bande-annonce-de-Trevor-GTA-5-recr%C3%A9%C3%A9e-dans-Les.jpg" roundedCircle />
           </Dropdown.Toggle>
           <Dropdown.Menu>
             <Dropdown.Item href="#/action-1">Mon compte</Dropdown.Item>
@@ -49,7 +54,7 @@ const Header = () => (
             <span className="header__profil__notifications__number">2</span>
             <MdNotifications />
           </div>
-          <Image src="" roundedCircle />
+          <Image src="https://androidgadget.org/wp-content/uploads/2020/03/Regardez-la-bande-annonce-de-Trevor-GTA-5-recr%C3%A9%C3%A9e-dans-Les.jpg" roundedCircle />
           <Dropdown>
             <Dropdown.Toggle id="dropdown-basic">
               Trevor
@@ -61,6 +66,74 @@ const Header = () => (
           </Dropdown>
         </div>
 
+        <div className="header__friend">
+          <h5 className="header__friend__title">Mes 3 premiers Amis ajoutés</h5>
+          <div className="test">
+            <article className="header__friend__profil">
+              <div className="img">
+                <Image src="https://geeko.lesoir.be/wp-content/uploads/sites/58/2019/10/fifa20-afp.png" roundedCircle />
+              </div>
+              <div>
+                <p className="header__friend__profil__nickname">Dhôko</p>
+                <a href="#!" className="header__friend__profil__gamer"><MdPerson /></a>
+                <a href="#!" className="header__friend__profil__message"><MdMessage /></a>
+              </div>
+            </article>
+            <article className="header__friend__profil">
+              <div className="img">
+                <Image src="https://geeko.lesoir.be/wp-content/uploads/sites/58/2019/10/fifa20-afp.png" roundedCircle />
+              </div>
+              <div>
+                <p className="header__friend__profil__nickname">Dhôko</p>
+                <a href="#!" className="header__friend__profil__gamer"><MdPerson /></a>
+                <a href="#!" className="header__friend__profil__message"><MdMessage /></a>
+              </div>
+            </article>
+            <article className="header__friend__profil">
+              <div className="img">
+                <Image src="https://geeko.lesoir.be/wp-content/uploads/sites/58/2019/10/fifa20-afp.png" roundedCircle />
+              </div>
+              <div>
+                <p className="header__friend__profil__nickname">Dhôko</p>
+                <a href="#!" className="header__friend__profil__gamer"><MdPerson /></a>
+                <a href="#!" className="header__friend__profil__message"><MdMessage /></a>
+              </div>
+            </article>
+          </div>
+
+          <div className="header__game">
+            <h5 className="header__game__title">Mes derniers jeux ajoutés</h5>
+            <div>
+              <article className="header__game__img">
+                <a href="#!">
+                  <Image src="https://geeko.lesoir.be/wp-content/uploads/sites/58/2019/10/fifa20-afp.png" rounded />
+                </a>
+              </article>
+              <article className="header__game__img">
+                <a href="#!">
+                  <Image src="https://geeko.lesoir.be/wp-content/uploads/sites/58/2019/10/fifa20-afp.png" rounded />
+                </a>
+              </article>
+              <article className="header__game__img">
+                <a href="#!">
+                  <Image src="https://geeko.lesoir.be/wp-content/uploads/sites/58/2019/10/fifa20-afp.png" rounded />
+                </a>
+              </article>
+              <article className="header__game__img">
+                <a href="#!">
+                  <Image src="https://geeko.lesoir.be/wp-content/uploads/sites/58/2019/10/fifa20-afp.png" rounded />
+                </a>
+              </article>
+            </div>
+          </div>
+
+          <div className="header__console">
+            <h5 className="header__console__title">Mes plateformes</h5>
+            <a href="#!" className="header__console__link">
+              <FaPlaystation />
+            </a>
+          </div>
+        </div>
       </Navbar.Collapse>
     </Navbar>
   </div>
