7c88d1bf86a5c650e7ece3d00633a5bd6edbc78a
Maxime Gruchet <maxime.gruchet@gmail.com>
2020-12-01 11:27:49 +0100

    Mise en place  du composant MenuFooter responsive

--- src/components/Olobby/index.js
+++ src/components/Olobby/index.js
@@ -7,11 +7,13 @@ import './styles.scss';
 
 // == Import
 import Header from 'src/components/Header';
+import MenuFooter from 'src/components/MenuFooter';
 
 // == Composant
 const Olobby = () => (
   <div className="olobby">
     <Header />
+    <MenuFooter />
   </div>
 );
 
