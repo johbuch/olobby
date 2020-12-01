dca0cb07b8e6b90c0102d1536cea28da0518fe17
johbuch <johan.buchert@gmail.com>
2020-12-01 12:01:00 +0100

    ajout email inscription

--- backend/src/Controller/Registration/RegistrationController.php
+++ backend/src/Controller/Registration/RegistrationController.php
@@ -5,9 +5,11 @@ namespace App\Controller\Registration;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\LoginFormAuthenticator;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
@@ -17,7 +19,7 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator, MailerInterface $mailer): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -40,6 +42,18 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+            $email = (new TemplatedEmail())
+                ->from('inscription@lobby.com')
+                ->to($user->getEmail())
+                ->subject('Bonjour ' . $user->getPseudo() . ', confirmation de votre inscription O\'Lobby')
+                ->htmlTemplate('emails/registration.html.twig')
+                ->context([
+                    'pseudo' => $user->getPseudo(),
+                    'e_mail' => $user->getEmail(),
+                ])
+            ;
+
+            $mailer->send($email);
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
