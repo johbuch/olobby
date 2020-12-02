da61f3eb1a799934e0c2da8cbbd39a484a7011a6
tristanlaulanie <tristanlaulanie@yahoo.fr>
2020-12-02 15:30:54 +0100

    Avancement Section

--- src/components/SectionHomeDisconnected/index.js
+++ src/components/SectionHomeDisconnected/index.js
@@ -9,7 +9,7 @@ const SectionHomeDisconnected = () => (
       <p className="section__text"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure ducimus sint expedita esse sunt tempore quas nihil eligendi nostrum, aperiam ipsa est ad quam quia placeat fugiat officiis corrupti. Deleniti similique modi ut consequatur eius vitae corrupti ullam cumque ratione!
       </p>
       <img
-        className="section__image"
+        className="section__image1"
         alt="imagesection1"
         src="https://img.huffingtonpost.com/asset/5cf512a3250000310adbe3b0.jpeg?ops=scalefit_960_noupscale"
       />
