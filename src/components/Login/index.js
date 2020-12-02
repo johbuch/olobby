c8e0db60bc3c4d352a56b3b5a5aa4da512e49cd1
Maxime Gruchet <maxime.gruchet@gmail.com>
2020-12-02 12:05:37 +0100

    Rmise en forme de l'indentation

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -8,67 +8,63 @@ import Form from 'react-bootstrap/Form'
 import './login.scss';
 
 const Login = () => {
-    function MyVerticallyCenteredModal(props) {
-        return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-                O'lobby logo  -  Se connecter à O'Lobby
-            </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Identifiant</Form.Label>
-                    <Form.Control className="input_color" type="email" placeholder="Adresse Email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            O'lobby logo  -  Se connecter à O'Lobby
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Identifiant</Form.Label>
+              <Form.Control className="input_color" type="email" placeholder="Adresse Email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Mot de passe</Form.Label>
-                    <Form.Control className="input_color" type="password" placeholder="mot de passe" />
-                </Form.Group>
-            
-                <Button className="btn_login" variant="primary" type="submit">
-                    Se connecter
-                </Button>
-                <div className="linechoice">
-                    <div className="linechoiceleft"></div>   
-                    <p className="choice">Pas de compte ? clique ci-dessous</p>
-                    <div className="linechoiceright"></div>
-                </div>
-                <Button className="btn_createAccount" type="submit">
-                    S'inscrire
-                </Button>
-            </Form>
-            </Modal.Body>
-        </Modal>
-        );
-    }
-    
-        const [modalShow, setModalShow] = React.useState(false);
-    
-        return (
-        <>
-            <Button variant="primary" onClick={() => setModalShow(true)}>
-            Launch Login modal
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Mot de passe</Form.Label>
+              <Form.Control className="input_color" type="password" placeholder="mot de passe" />
+            </Form.Group>
+            <Button className="btn_login" variant="primary" type="submit">
+              Se connecter
             </Button>
-    
-            <MyVerticallyCenteredModal
-            show={modalShow}
-            onHide={() => setModalShow(false)}
-            />
-        </>
-        );
-  
-  render(<App />);
+            <div className="linechoice">
+              <div className="linechoiceleft"></div>
+              <p className="choice">Pas de compte ? clique ci-dessous</p>
+              <div className="linechoiceright"></div>
+            </div>
+              <Button className="btn_createAccount" type="submit">
+                S'inscrire
+              </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch Login modal
+      </Button>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
 };
 
 export default Login;
