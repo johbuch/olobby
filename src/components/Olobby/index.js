562722f90690f7750083b00b325d65e24d003816
tristanlaulanie <tristanlaulanie@yahoo.fr>
2020-12-03 11:36:19 +0100

    HomeDisconnected sans navbar, postionnement et responsive en cours

--- src/components/Olobby/index.js
+++ src/components/Olobby/index.js
@@ -6,14 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.scss';
 
 // == Import
-import Header from 'src/components/Header';
 import HeaderHomeDisconnected from 'src/components/HeaderHomeDisconnected';
 import SectionHomeDisconnected from 'src/components/SectionHomeDisconnected';
 
 // == Composant
 const Olobby = () => (
   <div className="olobby">
-    <Header />
     <HeaderHomeDisconnected />
     <SectionHomeDisconnected />
   </div>
