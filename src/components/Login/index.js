04cdb7de7fad9e76854d2c93d13f30decb63f7e2
Marillier Corentin <corentin@MacBook-Pro-de-Marillier.local>
2020-12-03 17:44:41 +0100

    mise en place du flip card sur le login pour l'inscription

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import ReactCardFlip from "react-card-flip";
 
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
@@ -20,6 +21,13 @@ const Login = ({
   const handleSubmit = (evt) => {
     evt.preventDefault();
   };
+
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+  
   return (
     <>
       <Modal
@@ -29,39 +37,77 @@ const Login = ({
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
-        <Modal.Header closeButton className="modal__header">
-          <Modal.Title id="contained-modal-title-vcenter">
-            O'lobby logo  -  Se connecter à O'Lobby
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="modal__body">
-          <Form onSubmit={handleSubmit}>
-            <Field
-              name="email"
-              placeholder="Adresse Email"
-              onChange={changeField}
-              value={email}
-            />
-            <Field
-              name="password"
-              type="password"
-              placeholder="Mot de passe"
-              onChange={changeField}
-              value={password}
-            />
-            <Button className="btn_login" variant="primary" type="submit">
-              Se connecter
-            </Button>
-          </Form>
-          <div className="linechoice">
-            <div className="linechoiceleft"></div>
-            <p className="choice">Pas de compte ? clique ci-dessous</p>
-            <div className="linechoiceright"></div>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+          <div>
+            <Modal.Header closeButton className="modal__header">
+              <Modal.Title id="contained-modal-title-vcenter">
+                O'lobby logo  -  Se connecter à O'Lobby
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="modal__body">
+              <Form onSubmit={handleSubmit}>
+                <Field
+                  name="email"
+                  placeholder="Adresse Email"
+                  onChange={changeField}
+                  value={email}
+                />
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="Mot de passe"
+                  onChange={changeField}
+                  value={password}
+                />
+                <Button className="btn_login" variant="primary" type="submit">
+                  Se connecter
+                </Button>
+              </Form>
+              <div className="linechoice">
+                <div className="linechoiceleft"></div>
+                <p className="choice">Pas de compte ? clique ci-dessous</p>
+                <div className="linechoiceright"></div>
+              </div>
+              <Button onClick={handleClick} className="btn_createAccount" type="submit">
+                S'inscrire
+              </Button>
+            </Modal.Body>
           </div>
-          <Button className="btn_createAccount" type="submit">
-            S'inscrires
-          </Button>
-        </Modal.Body>
+          <div>
+          <Modal.Header closeButton className="modal__header">
+              <Modal.Title id="contained-modal-title-vcenter">
+                O'lobby logo  -  Se connecter à O'Lobby
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="modal__body">
+              <Form onSubmit={handleSubmit}>
+                <Field
+                  name="email"
+                  placeholder="Adresse Email"
+                  onChange={changeField}
+                  value={email}
+                />
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="Mot de passe"
+                  onChange={changeField}
+                  value={password}
+                />
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="Mot de passe"
+                  onChange={changeField}
+                  value={password}
+                />
+              </Form>              
+              <Button onClick={handleClick} className="btn_createAccount" type="submit">
+                Valider l'inscription
+              </Button>
+            </Modal.Body>
+          </div>
+        </ReactCardFlip>
       </Modal>
     </>
   );
@@ -73,6 +119,7 @@ Login.propTypes = {
   changeField: PropTypes.func.isRequired,
   modalShow: PropTypes.bool.isRequired,
   setModalShow: PropTypes.func.isRequired,
+  handleLogin: PropTypes.func.isRequired,
 };
 
 export default Login;
