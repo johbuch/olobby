abbb5c0478fd60ede8d457a2c42dd095bfc95dd5
Marillier Corentin <corentin@MacBook-Pro-de-Marillier.local>
2020-12-04 14:05:25 +0100

    mise en place du composant Annuaire

---
+++ src/components/Annuaire/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// == Import Bootstrap
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+
+// == Import scss
+import './annuaire.scss';
+
+const Annuaire= () => {
+  
+};
+
+export default Annuaire;
