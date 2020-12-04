40fc8838bf09ebab2f6c3c4e6e0339245386757c
tristanlaulanie <tristanlaulanie@yahoo.fr>
2020-12-04 09:12:16 +0100

    Intégration basique Home Disconnected avec React Bootstrap : OK

--- src/components/SectionHomeDisconnected/index.js
+++ src/components/SectionHomeDisconnected/index.js
@@ -10,42 +10,46 @@ const SectionHomeDisconnected = () => (
   <div>
     <section className="section">
       <Container>
-        <Row>
-          <Col sm md lg>
-            <div className="section__img">
-              <img
-                className="section__img__image1"
-                alt="imagesection1"
-                src="https://img.huffingtonpost.com/asset/5cf512a3250000310adbe3b0.jpeg?ops=scalefit_960_noupscale"
-              />
-            </div>
-          </Col>
-          <Col sm md lg>
-            <div className="section__text">
-              <h1 className="section__title">Titre</h1>
-              <p className="section__paragraph"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure ducimus sint expedita esse sunt tempore quas nihil eligendi nostrum, aperiam ipsa est ad quam quia placeat fugiat officiis corrupti. Deleniti similique modi ut consequatur eius vitae corrupti ullam cumque ratione!
-              </p>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm md lg>
-            <div className="section__text">
-              <h1 className="section__title">Titre</h1>
-              <p className="section__paragraph"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure ducimus sint expedita esse sunt tempore quas nihil eligendi nostrum, aperiam ipsa est ad quam quia placeat fugiat officiis corrupti. Deleniti similique modi ut consequatur eius vitae corrupti ullam cumque ratione!
-              </p>
-            </div>
-          </Col>
-          <Col sm md lg>
-            <div className="section__img">
-              <img
-                className="section__img__image1"
-                alt="imagesection2"
-                src="https://ak.picdn.net/shutterstock/videos/1020931030/thumb/1.jpg"
-              />
-            </div>
-          </Col>
-        </Row>
+        <div className="section__content1">
+          <Row>
+            <Col sm md lg>
+              <div className="section__img">
+                <img
+                  className="section__img__image1"
+                  alt="imagesection1"
+                  src="https://img.huffingtonpost.com/asset/5cf512a3250000310adbe3b0.jpeg?ops=scalefit_960_noupscale"
+                />
+              </div>
+            </Col>
+            <Col sm md lg>
+              <div className="section__text">
+                <h1 className="section__title">Titre</h1>
+                <p className="section__paragraph"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure ducimus sint expedita esse sunt tempore quas nihil eligendi nostrum, aperiam ipsa est ad quam quia placeat fugiat officiis corrupti. Deleniti similique modi ut consequatur eius vitae corrupti ullam cumque ratione!
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <div className="section__content2">
+          <Row>
+            <Col sm md lg>
+              <div className="section__text">
+                <h1 className="section__title">Titre</h1>
+                <p className="section__paragraph"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure ducimus sint expedita esse sunt tempore quas nihil eligendi nostrum, aperiam ipsa est ad quam quia placeat fugiat officiis corrupti. Deleniti similique modi ut consequatur eius vitae corrupti ullam cumque ratione!
+                </p>
+              </div>
+            </Col>
+            <Col sm md lg>
+              <div className="section__img">
+                <img
+                  className="section__img__image1"
+                  alt="imagesection2"
+                  src="https://ak.picdn.net/shutterstock/videos/1020931030/thumb/1.jpg"
+                />
+              </div>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </section>
   </div>
