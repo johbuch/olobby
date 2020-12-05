8c8fd9e8799a1576d309eb6a0ace965c5e3c650f
stankraken <mistrettastanislas@gmail.com>
2020-12-05 03:20:21 +0100

    custom token ok avec pseudo et avatar

---
+++ backend/src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthenticationSuccessListener
+{
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+    
+    /**
+     * @param AuthenticationSuccessEvent $event
+     */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $data['data'] = array(
+        'pseudo' => $user->getPseudo(),
+        'avatar' => $user->getAvatar(),
+        );
+
+        $event->setData($data);
+    }
+}
