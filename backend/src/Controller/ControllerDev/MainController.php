ee0dae7eaf81bfe40b21b9c2ac992330127c69c4
stankraken <mistrettastanislas@gmail.com>
2020-12-07 01:00:36 +0100

    test add friend

--- backend/src/Controller/ControllerDev/MainController.php
+++ backend/src/Controller/ControllerDev/MainController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller\ControllerDev;
 
+use App\Entity\Friend;
 use App\Entity\User;
+use App\Form\FriendType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -29,5 +32,27 @@ class MainController extends AbstractController
     public function dasboard(): Response
     {
         return $this->render('main/index.html.twig');
+    }
+
+    /**
+     * @Route("add/friend", name="add_friend", methods={"POST"})
+     */
+    public function addFriend(Request $request, UserRepository $userRepository): Response
+    {
+
+        $friend = new Friend();
+
+        $form = $this->createForm(FriendType::class, $friend, ['csrf_protection' => false]);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $friend->setCreatedAt(new \DateTime());
+            $friend->setUpdatedAt(new \DateTime());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($friend);
+            $entityManager->flush();
+        } 
+        return $this->redirectToRoute('main_browse');
     }
 }
