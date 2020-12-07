64c73ea9fe3c533243ebc3dcc19a91aa5aa442b9
Maxime Gruchet <maxime.gruchet@gmail.com>
2020-12-07 15:49:11 +0100

    WIP

--- src/middlewares/players.js
+++ src/middlewares/players.js
@@ -6,7 +6,7 @@ const playersMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case FETCH_PLAYERS:
       console.log('middleware, action FETCH_PLAYERS');
-      axios.get('http://ec2-52-3-54-243.compute-1.amazonaws.com/api/v1/users', {
+      axios.get('http://ec2-52-3-54-243.compute-1.amazonaws.com/api/v1/platforms', {
         headers: { Authorization: `Bearer ${window.localStorage.getItem('token')}` },
       }, {
         withCredentials: true,
