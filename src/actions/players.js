852f46968ad1902e12bde721ce53b660255bc57d
Maxime Gruchet <maxime.gruchet@gmail.com>
2020-12-12 10:34:05 +0100

    Mise en place middleware + action pour le matchmaking

--- src/actions/players.js
+++ src/actions/players.js
@@ -1,8 +1,14 @@
 export const FETCH_PLAYERS = 'FETCH_PLAYERS';
 export const SAVE_PLAYERS = 'SAVE_PLAYERS';
 
+export const FETCH_PLAYERS_MATCHMAKING = 'FETCH_PLAYERS_MATCHMAKING';
+
 export const fetchPlayers = () => ({
   type: FETCH_PLAYERS,
+});
+
+export const fetchMatchmaking = () => ({
+  type: FETCH_PLAYERS_MATCHMAKING,
 });
 
 export const savePlayers = (players) => ({
--- src/middlewares/players.js
+++ src/middlewares/players.js
@@ -1,12 +1,25 @@
 import axios from 'axios';
 
-import { FETCH_PLAYERS, savePlayers } from 'src/actions/players';
+import { FETCH_PLAYERS, savePlayers, FETCH_PLAYERS_MATCHMAKING } from 'src/actions/players';
 
 const playersMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case FETCH_PLAYERS:
       console.log('middleware, action FETCH_PLAYERS');
       axios.get('http://ec2-52-3-54-243.compute-1.amazonaws.com/api/v1/platforms', {
+        headers: { Authorization: `Bearer ${window.localStorage.getItem('token')}` },
+      }, {
+        withCredentials: true,
+      })
+        .then((response) => {
+          store.dispatch(savePlayers(response.data));
+        })
+        .catch((error) => {
+        });
+      next(action);
+      break;
+    case FETCH_PLAYERS_MATCHMAKING:
+      axios.get('http://ec2-52-3-54-243.compute-1.amazonaws.com/api/v1/matchmaking', {
         headers: { Authorization: `Bearer ${window.localStorage.getItem('token')}` },
       }, {
         withCredentials: true,
