cd3cfcd9d73cede6d0d56cd0c56ec8918b1ab495
johbuch <johan.buchert@gmail.com>
2020-12-14 14:03:33 +0100

    modification des routes du MatchmakingController pour rajouter l'ID en paramètre de l'URL

--- backend/src/Controller/Api/V1/Matchmaking/MatchmakingController.php
+++ backend/src/Controller/Api/V1/Matchmaking/MatchmakingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Api\V1\Matchmaking;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,11 +14,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class MatchmakingController extends AbstractController
 {
     /**
-     * @Route("/levelandplatformandvideogame", name="api_v1_matchmaking_levelAndPlatformAndVideogame", methods={"GET"})
+     * @Route("/levelandplatformandvideogame/{id}", name="api_v1_matchmaking_levelAndPlatformAndVideogame", methods={"GET"})
      * méthode qui recommande des users en filtrant en fonction du niveau du user connecté,
      * de sa plateforme et de son 1er jeux vidéo qu'il a choisi
      */
-    public function levelAndPlatformAndVideogame(UserRepository $userRepository): Response
+    public function levelAndPlatformAndVideogame(User $user, UserRepository $userRepository): Response
     {
         // récupération du user connecté
         $user = $this->getUser();
@@ -35,11 +36,11 @@ class MatchmakingController extends AbstractController
     }
 
     /**
-     * @Route("/platformandvideogame", name="api_v1_matchmaking_platformAndVideogame", methods={"GET"})
+     * @Route("/platformandvideogame/{id}", name="api_v1_matchmaking_platformAndVideogame", methods={"GET"})
      * méthode qui recommande des users en filtrant en fonction de la plateforme du user connecté
      * et du 2ème jeux vidéo qu'il a ajouté à son profil
      */
-    public function platformAndVideogame(UserRepository $userRepository): Response
+    public function platformAndVideogame(User $user, UserRepository $userRepository): Response
     {
         // récupération du user connecté
         $user = $this->getUser();
