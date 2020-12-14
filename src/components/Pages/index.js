c1b670897ce4ef9d300899d6202c92ecc88a3013
tristanlaulanie <tristanlaulanie@yahoo.fr>
2020-12-14 14:29:37 +0100

    Suppression des imports vides

--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,31 +1,18 @@
 // == Import npm
-import React,{ useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 
 // == Import
 import Annuaire from 'src/components/Annuaire';
 import CardProfile from 'src/containers/CardProfile';
 
-import EditProfile from 'src/containers/EditProfile';
-
-import Card from 'react-bootstrap/Card';
-import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-
-// == Import icons
-import { MdGroupAdd } from 'react-icons/md';
-import { FaPlaystation } from 'react-icons/fa';
 
 
 // == Import scss
 import './pages.scss';
 
 // == Composant
-const Pages = ({launchFetchPlayers, players}) => {
+const Pages = ({ launchFetchPlayers, players }) => {
     useEffect(() => {
         launchFetchPlayers();
       }, []);
@@ -35,51 +22,36 @@ const Pages = ({launchFetchPlayers, players}) => {
 return (    
   <div className="pages">
     <Route
-      path="/"
-      exact
-    >
-      <CardProfile />
+        path="/">
     </Route>
-    <Route
+        <Route
         path="/annuaire-de-joueur">
         <Annuaire />
         <h1 className="title_pages">Annuaire de joueurs</h1>
         {players.map((player) => (            
             <CardProfile key={player.id} {...player}/>
-        ))}        
+        ))}
+    </Route>
+        <Route
+        path="/annuaire-de-jeux">
+        <Annuaire />
+        <h1 className="title_pages">Annuaire de jeux</h1>
     </Route>  
     <Route
-      path="/annuaire-de-jeux"
-    >
-      <Annuaire />
-      <h1 className="title_pages">Annuaire de jeux</h1>
-      <CardProfile />
+        path="/annuaire-de-plateformes">
+        <Annuaire />
+        <h1 className="title_pages">Annuaire de plateformes</h1>
     </Route>
     <Route
-      path="/annuaire-de-plateformes"
-    >
-      <Annuaire />
-      <h1 className="title_pages">Annuaire de plateformes</h1>
-      <CardProfile />
+        path="/mes-jeux">
+        <h1 className="title_pages2">Mes jeux</h1>
     </Route>
     <Route
-      path="/mes-jeux"
-    >
-      <h1 className="title_pages2">Mes jeux</h1>
+        path="/mes-amis">
+        <h1 className="title_pages2">Mes amis</h1>
     </Route>
-    <Route
-      path="/mes-amis"
-    >
-      <h1 className="title_pages2">Mes amis</h1>
-    </Route>
-    <Route
-      path="/mon-compte"
-    >
-      <EditProfile />
-    </Route>
-  </div>
-);
-
+    </div>
+)};
 
 // == Export
 export default Pages;
