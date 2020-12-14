fa52403f341769fb9e4c485d73add97842637cad
stankraken <mistrettastanislas@gmail.com>
2020-12-14 16:43:58 +0100

    +1

--- backend/src/DataFixtures/NelmioAlice.php
+++ backend/src/DataFixtures/NelmioAlice.php
@@ -4,14 +4,23 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
 
-class NelmioAlice extends Fixture
+class NelmioAliceFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $em)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $loader = new NativeLoader();
 
-        $manager->flush();
+        //importe le fichier de fixtures et récupère les entités générés
+        $entities = $loader->loadFile(__DIR__.'/fixtures.yaml')->getObjects();
+
+        //empile la liste d'objet à enregistrer en BDD
+        foreach ($entities as $entity) {
+            $em->persist($entity);
+        }
+
+        //enregistre
+        $em->flush();
     }
 }
