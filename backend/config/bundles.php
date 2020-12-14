77bc3ecc704e90a716baacecf9b0f21afb401901
stankraken <mistrettastanislas@gmail.com>
2020-12-14 16:41:10 +0100

    install nelmio

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
