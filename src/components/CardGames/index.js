379421b042c30a078bf42f06d06f04d88a1c6b8a
Marillier Corentin <corentin@MacBook-Pro-de-Marillier.local>
2020-12-15 14:21:23 +0100

    modif css et ajout d'informations

--- src/components/CardGames/index.js
+++ src/components/CardGames/index.js
@@ -4,10 +4,7 @@ import PropTypes from 'prop-types';
 // == Import Bootstrap
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
-
-// == Import icons
-import { MdGroupAdd } from 'react-icons/md';
+import Col from 'react-bootstrap/Col';
 
 // == Import scss
 import './cardGames.scss';
@@ -15,31 +12,28 @@ import './cardGames.scss';
 const CardGames = (props) => {
 
   const [modalShow, setModalShow] = React.useState(false);
-  const { title, id, image } = props;
+  const { title, id, image, typeGame, platforms } = props;
 
   return (
-    <div className="cardProfile">
-      <Card style={{ width: '18rem' }}>
-        <Button className="btn-add-friend"><MdGroupAdd /></Button>
-          <Card.Img variant="top" src={image} />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">{title}</Card.Subtitle>
-          <p>Ces 3 jeux Favoris</p>
-          <div className="profileGame">
-            <div className="img">
-              <Image src="https://www.journaldugeek.com/content/uploads/2019/10/cod-640x360.jpg" rounded />
+    <Col xs={12} md={8} lg={4}>
+      <div className="cardProfile">
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">{ typeGame !== null ? typeGame.name : "non défini" }</Card.Subtitle>
+            <p>plateforme(s)</p>
+            <div className="profileGame">
+              <div className="img_plat">
+                {platforms.map((platform) => (
+                  <Image className="img_plat" src={ platform !== null ? platform.image : "erreur" } key={platform.id} {...platform} rounded />
+                ))}                
+              </div>
             </div>
-            <div className="img">
-              <Image src="https://www.journaldugeek.com/content/uploads/2019/10/cod-640x360.jpg" rounded />
-            </div>
-            <div className="img">
-              <Image src="https://www.journaldugeek.com/content/uploads/2019/10/cod-640x360.jpg" rounded />
-            </div>
-          </div>
-        </Card.Body>
-      </Card>
-    </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </Col>
   );
 };
 
--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -7,7 +7,9 @@ import Annuaire from 'src/components/Annuaire';
 import CardProfile from 'src/components/CardProfile';
 import CardGames from 'src/components/CardGames';
 
-
+// == Import Bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 // == Import scss
 import './pages.scss';
@@ -25,22 +27,29 @@ return (
     <Route
         path="/">
     </Route>
-        <Route
+    <Route
         path="/annuaire-de-joueur">
         <Annuaire />
         <h1 className="title_pages">Annuaire de joueurs</h1>
-        {players.map((player) => (            
-            <CardProfile key={player.id} {...player}/>
-        ))}
+        <Container className="cards_container">
+            <Row>
+                {players.map((player) => (            
+                    <CardProfile key={player.id} {...player}/>
+                ))}
+            </Row>
+        </Container>
     </Route>
-        <Route
+    <Route
         path="/annuaire-de-jeux">
         <Annuaire />
         <h1 className="title_pages">Annuaire de jeux</h1>
-        {games.map((game) => (            
-            <CardGames key={game.id} {...game}/>
-        ))}
-        
+        <Container>
+            <Row>
+                {games.map((game) => (            
+                    <CardGames key={game.id} {...game}/>
+                ))}
+            </Row>
+        </Container>
     </Route>  
     <Route
         path="/annuaire-de-plateformes">
